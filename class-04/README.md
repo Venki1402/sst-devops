bbaba019fa7285587f538a22c5286e125fff57d9
Venki1402 <saivenkatesh.alampally@gmail.com>
2025-11-09 19:00:56 +0530

    feat: add notes class 04

--- class-04/README.md
+++ class-04/README.md
@@ -2,4 +2,7 @@
 
 - linux architecture -- hardware < kernal < shell < user programs
 - #! - used for shebang, # used for comment
--   
+- pass - `user@123!` -- for root
+- `sudo su` -> go to super user
+- `su - 'devs'` -> switch user
+- `set -e` -> Immediately exit when there is an error
--- class-04/user_group.sh
+++ class-04/user_group.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
+#any time there is an error - immediately exit
+set -e
+
+#check if root user is executing 
+if [[ SEUID -ne 2 ]]; then
+    echo "This script execution requires sudo privileges"
+    exit 1 #exit with error code 1
+fi
+
+if [[ $# -ne 2 ]]; then
+    echo "Usage; $0 <username> < groupname>"
+    exit 1 #exit with error code 1
+fi
+
 username="$1"
 groupname="$2"
+
 
 groupadd "$groupname"
 useradd -m -s /bin/bash -g "$groupname" "$username"
